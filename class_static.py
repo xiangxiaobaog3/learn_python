8f6e6a28107ef8b7fa4d82eb0dff933419ac090a
xiangxiaobao <943300866@qq.com>
2016-09-29 14:27:13 +0800

    静态方法用例

---
+++ class_static.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+# 静态方法可以被类直接调用,可以被所有的实例化对象共享,静态方法并没有和类的实例进行名称绑定,相当于全局函数
+
+class Fruit:
+    price = 0
+
+    def __init__(self):
+        self.__color = "red"
+
+    def getColor(self):
+        print self.__color
+
+    @staticmethod
+    def getPrice():
+        print Fruit.price
+
+    def __getPrice():
+        Fruit.price += 10
+        # print Fruit.price
+
+    count = staticmethod(__getPrice)
+
+if __name__ == '__main__':
+    apple = Fruit()
+    apple.getColor()
+    Fruit.count()
+    Fruit.getPrice()
